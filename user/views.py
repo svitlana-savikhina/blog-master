a5d6656c943be275dd06df813da0e9182a3d311d
Svitlana Savikhina <savihina111@gmail.com>
2024-05-30 22:07:31 +0300

    Add views for user creation and profile detail

--- user/views.py
+++ user/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# Create your views here.
+from user.models import Profile
+from user.serializers import UserCreateSerializer, ProfileSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserCreateSerializer
+    permission_classes = [AllowAny]
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserCreateSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
+
+
+class ProfileDetailView(generics.RetrieveUpdateAPIView):
+    serializer_class = ProfileSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        profile, created = Profile.objects.get_or_create(user=self.request.user)
+        return self.request.user
