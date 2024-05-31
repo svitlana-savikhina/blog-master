d79fd81b27750360edb9a9ceb3c424cc0e941157
Svitlana Savikhina <savihina111@gmail.com>
2024-05-31 20:53:54 +0300

    Add API schema and documentation endpoints

--- blog_master/urls.py
+++ blog_master/urls.py
@@ -19,6 +19,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -26,4 +31,13 @@ urlpatterns = [
     path("api/user/", include("user.urls", namespace="user")),
     path("api/profile/", include("user.urls", namespace="profile")),
     path("api/auth/", include("dj_rest_auth.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/doc/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
